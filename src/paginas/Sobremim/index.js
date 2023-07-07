c25c8caef528d5b545cf097d6e4e841f502406f4
Nickolas Amaral <125821680+nickolasamaral03@users.noreply.github.com>
2023-07-07 09:23:51 -0300

    Modificando o sobre mim

--- src/paginas/Sobremim/index.js
+++ src/paginas/Sobremim/index.js
@@ -22,24 +22,24 @@ export default function SobreMim(){
 
 
 <p className={styles.paragrafo}>
-            Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+            Oi, tudo bem? Eu sou Nickolas, aluno da Alura e estou feliz de te ver por aqui.
 </p>
 
 <p className={styles.paragrafo}>
-            Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
+           Minha história na programação começa 7 meses atrás, quando estava confuso em qual área progredir em minha carreira. Porém fiquei impressionado e admirado nos desenvolvedores e isso me cativou!
 </p>
 
 <p className={styles.paragrafo}>
-            No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles. 
+        Comecei a estudar programação logo depois, com algumas aulas no Youtebe e então, me dei conta que a Web não é só aquilo que usuário vê, mas sim, muito mais vasta 
 </p>
 <p className={styles.paragrafo}>
-            Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
+        Assim, comecei a estudar no Youtube e logo após na Alura com possibilidades de aprender muito já que é uma plataforma muita completa
 </p>
 <p className={styles.paragrafo}>
-            E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
+        E com 7 meses de estudo, tem uma noção boa já de JavaScript e React Js, além de CSS e HTML. Pretendo estudar Node Js ainda!
 </p>
 <p className={styles.paragrafo}>
-           Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
+        Irei começar a faculdade de Ciências da Computação no meio desse ano, após ter uma boa nota no Enem, com esse curso superior, meu conhecimento sobre computadores se tornará mais completo!
 </p>
         </PostModelo>
         )
